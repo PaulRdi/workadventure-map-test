3eaee917ead1a0f9d5e881fa55ed723eeae891ea
TheBurningGiraffe <79365489+TheBurningGiraffe@users.noreply.github.com>
2021-03-10 00:23:23 +0000

    Deploying to gh-pages from master @ 835dafd524337c862e72a0e010e38554601fcf64 🚀

---
+++ tsx/Interiors_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Interiors_32x32" tilewidth="32" tileheight="32" tilecount="8272" columns="16">
+ <image source="../../../../Desktop/UFIS2021/tileset/Modern_Interiors/1_Interiors/32x32/Interiors_32x32.png" width="512" height="16544"/>
+</tileset>
