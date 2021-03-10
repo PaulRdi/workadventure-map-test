70d5d94100b3de605ffe07fa8934c099eb0063b3
TheBurningGiraffe <79365489+TheBurningGiraffe@users.noreply.github.com>
2021-03-10 18:43:06 +0000

    Deploying to gh-pages from master @ efc346392bb4b93ceb75cbeef6a877cf1333542b 🚀

---
+++ tsx/Interiors_32x32_v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Interiors_32x32_v2" tilewidth="32" tileheight="32" tilecount="8272" columns="16">
+ <image source="../Tiles/Interiors_32x32.png" width="512" height="16544"/>
+</tileset>
