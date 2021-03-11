71a8328e71112dcd1ef6c0b3048dc8dea4f8ecd1
TheBurningGiraffe <mitternachtsseele@gmail.com>
2021-03-11 17:17:55 +0100

    Integrated Booth2 into Prototype_v2

---
+++ tsx/8_Gym_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="8_Gym_32x32" tilewidth="32" tileheight="32" tilecount="304" columns="16">
+ <image source="../Tiles/8_Gym_32x32.png" width="512" height="608"/>
+</tileset>
