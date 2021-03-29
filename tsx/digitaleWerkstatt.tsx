e9858001167457b6fd4dfc2ec8a3ec59ae24b560
Elisabeth Harzbecker <elli.hopps@gmail.com>
2021-03-29 21:23:59 +0200

    Jobtrüffel und digitaleWerkstatt Booth + Layer clean up

---
+++ tsx/digitaleWerkstatt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="digitaleWerkstatt" tilewidth="32" tileheight="32" tilecount="45" columns="9">
+ <image source="../Tiles/Logos/LOGO_DIE_DIGITALWERKSTATT-scaled.png" width="288" height="160"/>
+</tileset>
