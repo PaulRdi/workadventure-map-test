ae4010b36a4920590a6846a8b308dcfd00c329f4
Anthony Schwartz <schwartzanthonyj@gmail.com>
2021-05-01 16:01:16 -0500

    Hall3

--- tsx/8.tsx
+++ tsx/8.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="8" tilewidth="32" tileheight="32" tilecount="10" columns="5">
- <image source="../../../../Desktop/Images Expo/8.png" width="160" height="81"/>
+ <image source="../Tiles/Logos/8.png" width="160" height="81"/>
 </tileset>
