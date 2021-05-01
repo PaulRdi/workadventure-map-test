70ece90d8cb4effac41bf4f8fa481810728a4458
SchwartzAnthony <36740099+SchwartzAnthony@users.noreply.github.com>
2021-05-01 20:43:26 +0000

    Deploying to gh-pages from master @ f07fc2173449857dde022a036aec0c3404d67992 🚀

--- tsx/8.tsx
+++ tsx/8.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="8" tilewidth="32" tileheight="32" tilecount="45" columns="9">
- <image source="../../../../Desktop/Images Expo/8.jpg" width="316" height="160"/>
+<tileset version="1.4" tiledversion="1.4.3" name="8" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="../../../../Desktop/Images Expo/8.png" width="160" height="81"/>
 </tileset>
