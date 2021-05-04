0ed006c1a317fd049eed576c70da043ffef34fb8
SchwartzAnthony <36740099+SchwartzAnthony@users.noreply.github.com>
2021-05-04 13:21:47 +0000

    Deploying to gh-pages from master @ ee02be57a117657927c81d1edf1999e5a365df37 🚀

--- tsx/hall_numbers.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="hall_numbers" tilewidth="32" tileheight="32" tilecount="560" columns="16">
- <image source="../Tiles/hall_numbers.png" width="512" height="1120"/>
-</tileset>
