336eccc0347d7328721a20f47c408f04527ea5c2
Anthony Schwartz <schwartzanthonyj@gmail.com>
2021-05-04 07:51:05 -0500

    Hall3

---
+++ tsx/drpfleger_logo_changed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="drpfleger_logo_changed" tilewidth="32" tileheight="32" tilecount="20" columns="10">
+ <image source="../Tiles/Logos/drpfleger_logo_changed.png" width="320" height="76"/>
+</tileset>
---
+++ tsx/drpfleger_logo_changed1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="drpfleger_logo_changed1" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="../Tiles/Logos/drpfleger_logo_changed1.png" width="160" height="76"/>
+</tileset>
